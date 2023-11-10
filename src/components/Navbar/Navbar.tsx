c19c96fa6ed9c4e28d1142015f3b84f2a9b2bac1
Spencer <35084829+Spencer-Mosley@users.noreply.github.com>
2023-11-10 10:47:25 -0600

    updated navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,15 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
+import { Flex, Image } from '@chakra-ui/react';
 
-
-const Navbar : React.FC = () => {
-    return <div>Navbar</div>;
+const Navbar: React.FC = () => {
+    return (
+        <Flex bg="white" height="44px" padding="6px 12px">
+            <Flex align="center">
+                <Image src="/images/redditFace.svg" height="30px" />
+                <Image src="/images/redditText.svg" height="46px" display={{ base: 'none', md: 'unset' }} />
+            </Flex>
+        </Flex>
+    );
 };
 
 export default Navbar;
